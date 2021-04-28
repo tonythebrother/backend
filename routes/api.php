9c5aac3dcd6a34e05eceb806ccc53d3fb296dd89
Ronny Yoel Pérez <ronnyyoelperez@gmail.com>
2021-04-28 01:41:29 -0400

    Asigned api route, Cliente, Empleado, Productos

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\EmpleadoController;
+use App\Http\Controllers\ProductosController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::apiResource('cliente', ClienteController::class);
+Route::apiResource('empleado', EmpleadoController::class);
+Route::apiResource('productos', ProductosController::class);
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
 
 Route::any('{all}',function(){
     return view('welcome'); // it should be main blade file 
